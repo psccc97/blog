c3a2eb6c6ff97828e2120d69d06cd1ae4461dfac
psccc97 <pascucci.lino@gmail.com>
2017-03-29 13:52:37 +0200

    modif de la navigation

--- Blog/view/Post.php
+++ Blog/view/Post.php
@@ -18,27 +18,16 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Blog</a>
+                    <a class="navbar-brand" href="../index.php">Blog</a>
                 </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-                        <li><a href="view/Post.php">Post</a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="../index.php">Home</a></li>
+                    <li><a href="view/Post.php">Post</a></li>
+                </ul>
+            </div>
         </nav>        
         <div class="container">
             <form class="form-group" action="../controllerPost.php" method="post" enctype="multipart/form-data">
@@ -49,3 +38,5 @@ and open the template in the editor.
         </div>
     </body>
 </html>
+<!--<li class="active"><a href="../index.php">Home <span class="sr-only">(current)</span></a></li>
+<li><a href="view/Post.php">Post<span class="sr-only"> (current)</span></a></li>-->
